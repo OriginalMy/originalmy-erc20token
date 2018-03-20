b49930a860c5450a6bf20b6fb15f70c15a93b119
osoriojr <osoriojr@linux.com>
2018-03-20 15:55:41 +0200

    Version bump

--- contracts/ABC.sol
+++ contracts/ABC.sol
@@ -17,7 +17,7 @@ contract ABC {
     string public name;                   //fancy name: eg Simon Bucks
     uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
     string public symbol;                 //An identifier: eg SBX
-    string public version = 'ABCv0.1';       //human 0.1 standard. Just an arbitrary versioning scheme.
+    string public version = 'ABCv1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
     address public owner;
     uint256 public totalSupply;
     
